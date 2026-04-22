c80dacba455a16197eb19ec1cfa3d77d37aefb40
mohan <mohan@mohans-MacBook-Air.local>
2026-04-23 00:45:21 +0530

    Final cleanup of URLs and media serving to resolve Render build failure

--- backend/exam_platform/urls.py
+++ backend/exam_platform/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from accounts.api_views import (
@@ -136,12 +137,7 @@ urlpatterns = [
     path('api/admin/trigger-cleanup/', trigger_data_cleanup, name='api-trigger-cleanup'),
 ]
 
-from django.views.static import serve
-
+# Manual media serving for production visibility
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    pass
